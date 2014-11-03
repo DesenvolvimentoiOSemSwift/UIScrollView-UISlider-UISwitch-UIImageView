bb92ce30e71f85f9bb0baf39728bf05956b09635
Mauricio Meirelles <mauricio.m@gmail.com>
2014-11-03 13:11:55 -0200

    First Commit

---
+++ Aula 7/DetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.swift
+//  Aula 7
+//
+//  Created by Mauricio Meirelles on 9/11/14.
+//  Copyright (c) 2014 Mauricio. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UIScrollViewDelegate {
+
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var onOffZoom: UISwitch!
+    
+    var imgSelected: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        scrollView.maximumZoomScale = 4
+        scrollView.minimumZoomScale = 1
+        scrollView.zoomScale = 1
+        
+
+        imageView.image = UIImage(named: imgSelected!)
+        imageView.sizeToFit()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        scrollView.contentSize = imageView.frame.size
+        
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView!) -> UIView!
+    {
+        return imageView
+    }
+    
+    @IBAction func changeValue(sender: UISlider) {
+        scrollView.zoomScale = CGFloat(slider.value)
+    }
+
+    func scrollViewDidZoom(scrollView: UIScrollView!)
+    {
+        slider.setValue(Float(scrollView.zoomScale), animated: true)
+    }
+    
+    @IBAction func turnOnOff(sender: AnyObject) {
+        slider.enabled = onOffZoom.on
+        
+        scrollView.pinchGestureRecognizer.enabled = onOffZoom.on
+    }
+    
+
+}
---
+++ Aula 7/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Aula 7
+//
+//  Created by Mauricio Meirelles on 9/11/14.
+//  Copyright (c) 2014 Mauricio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var imagesArray: [(name: String,image: String)] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagesArray = [(" WATCH","img1"),("iOS 8","img2"),(" PAY","img3")]
+        
+    
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return imagesArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var tableViewcell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        
+        tableViewcell.textLabel?.text = imagesArray[indexPath.row].name
+        
+        return tableViewcell;
+    }
+
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var dtVC = segue.destinationViewController as DetailViewController
+        
+        let idxPath = tableView.indexPathForCell(sender as UITableViewCell) as NSIndexPath?
+        
+        dtVC.imgSelected = imagesArray[idxPath!.row].image
+        
+    }
+
+
+
+}
+
